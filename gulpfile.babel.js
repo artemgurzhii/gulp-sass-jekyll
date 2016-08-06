a8f10aa601b952ddb4bc40fa3d437f4353375ce1
Artem Gurzhii <artem.gurzhii@gmail.com>
2016-08-06 15:16:06 +0300

    add comments and task destination folder is now passed to function as options object value

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,65 +1,75 @@
-import gulp             from 'gulp';
+import gulp from 'gulp';
 
-const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV === 'devlopment'; // NODE_ENV=production gulp
-
+// defining main paths for .sass/.js/.html etc files
 const paths = {
-  css:   'assets/css/main.sass',
-  js:    'assets/js/common.js',
-  jsM:   'assets/js/modules/*.js',
-  img:   'assets/img/*.*',
-  clean: '_site',
-  site:  '_site/index.html',
-  md:    'README.md'
+  css:    'assets/css/main.sass',
+  cssMin: 'assets/css/min',
+  js:     'assets/js/common.js',
+  jsMain: 'assets/js/modules/*.js',
+  jsMin:  'assets/js/min',
+  img:    'assets/img/*.*',
+  imgMin: 'assets/img/min/',
+  clean:  '_site',
+  site:   '_site/index.html',
+  md:     'README.md'
 }
 
+// write lazyRequireTask function to have all tasks in seperate files
+// arguments - name of task, path to the task file, path to the folder, where task should be executed
 let lazyRequireTask = (taskName, path, options = {}) => {
   options.taskName = taskName;
-  path = './gulp/' + path;
+  path = `./gulp/${path}`;
   gulp.task(taskName, callback => {
     let task = require(path).call(this, options);
     return task(callback);
   });
 };
 
+// Requiring tasks on development stage(CSS,JS,IMG)
 lazyRequireTask('assets:css', 'css', {
-  src: paths.css
+  src: paths.css,
+  dest: paths.cssMin
 });
-
 lazyRequireTask('assets:js', 'js', {
-  src: paths.js
+  src: paths.js,
+  dest: paths.jsMin
 });
-
 lazyRequireTask('assets:img', 'img', {
-  src: paths.img
+  src: paths.img,
+  dest: paths.imgMin
 });
 
+// Requiring tasks on production stage(CSS,JS,IMG,DOCS,HTML)
+lazyRequireTask('deploy:css', 'deploy/css', {
+  src: paths.css,
+  dest: paths.cssMin
+});
+lazyRequireTask('deploy:js', 'deploy/js', {
+  src: paths.js,
+  dest: paths.jsMin
+});
+lazyRequireTask('deploy:img', 'deploy/img', {
+  src: paths.img,
+  dest: paths.imgMin
+});
+lazyRequireTask('deploy:docs', 'deploy/docs', {
+  src: [paths.md, paths.jsMain]
+});
+lazyRequireTask('deploy:html', 'deploy/html', {
+  src: paths.site,
+  dest: paths.clean
+});
+
+// Others tasks
+// task to prevent caching(will remove _site folder where website is stored)
 lazyRequireTask('clean', 'clean', {
   src: paths.clean
 });
-
-lazyRequireTask('deploy:js', 'deploy/js', {
-  src: paths.js
-});
-
-lazyRequireTask('deploy:css', 'deploy/css', {
-  src: paths.css
-});
-
-lazyRequireTask('deploy:img', 'deploy/img', {
-  src: paths.img
-});
-
-lazyRequireTask('deploy:docs', 'deploy/docs', {
-  src: [paths.md, paths.jsM]
-});
-
-lazyRequireTask('deploy:html', 'deploy/html', {
-  src: paths.site
-});
-
+// Syncing and reloading browser
 lazyRequireTask('browser:sync', 'sync');
 lazyRequireTask('browser:build', 'build');
 
+// defining tasks to run parallel
 const browser = gulp.parallel('browser:sync', 'browser:build');
 const assets  = gulp.parallel('assets:css', 'assets:js', 'assets:img');
 const clean   = gulp.parallel('clean');
